f01f28643e40eeb262b616046d555283827b21b0
MariaBernal23 <marbernalgamez8@gmail.com>
2023-11-10 11:43:05 -0600

    subida 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:prueba2/firebase_options.dart';
+import 'package:prueba2/view/login.dart';
+import 'package:prueba2/view/signup.dart';
+import 'package:prueba2/firebase_options.dart';
+import 'package:prueba2/pages/home.dart';
 
-
-void main() async{
- WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
- );
- runApp(MyApp());
- }
-
-
-/* void main() {
+/*
+void main() {
   runApp(const MyApp());
-} */
+}*/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,22 +27,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Prueba1'),
+      initialRoute: "/login",
+      routes: {
+        "/home": (context) => const Home(),
+        "/login": (context) => const Login(),
+        "/signup": (context) => const Signup(),
+      },
+      //home: const MyHomePage(title: 'PROYECTO SAVER'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+/*class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
- 
-
   final String title;
 
   @override
@@ -48,7 +54,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-   void getData() async {
+  void getPrueba() async {
  CollectionReference collectionReference = 
 FirebaseFirestore.instance.collection("tb_prueba");
  QuerySnapshot mensajes = await collectionReference.get();
@@ -59,79 +65,44 @@ FirebaseFirestore.instance.collection("tb_prueba");
  }
  }
  }
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
+*/
+/*
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'PROYECTO APP DE DENUNCIAS:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: (){},
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       child: const Icon(Icons.add),
+      ),*/ // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getData();
-  }
+   @override
+ void initState() {
+ // TODO: implement initState
+ super.initState();
+ getPrueba();
+ }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+ @override
+ void dispose() {
+ // TODO: implement dispose
+ super.dispose();
+ }
 }
+*/
---
+++ lib/pages/home.dart
@@ -0,0 +1,344 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'packaprueba2
+import 'package:prueba2/firebase_options.dart';
+import 'package:prueba2/pages/Cruz.dart';
+import 'package:prueba2/pages/Police.dart';
+import 'package:prueba2/pages/acerca.dart';
+import 'package:prueba2/pages/bomberos.dart';
+import 'package:prueba2/pages/configuraciones.dart';
+import 'package:prueba2/pages/cruzverde.dart';
+import 'package:prueba2/pages/denuncias.dart';
+import 'package:prueba2/pages/fiscalia.dart';
+import 'package:prueba2/pages/fuerza.dart';
+import 'package:prueba2/pages/mapa.dart';
+import 'package:prueba2/pages/perfil.dart';
+import 'package:prueba2/pages/procuraduria.dart';
+import 'package:prueba2/pages/proteccion.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+   // Función para cerrar sesión
+  void _signOut(BuildContext context) {
+    // Aquí debes realizar las acciones necesarias para cerrar la sesión, como desautenticar al usuario.
+    // Luego, puedes navegar a la pantalla de inicio de sesión o a donde desees.
+    // Ejemplo con Firebase:
+    // FirebaseAuth.instance.signOut();
+    // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        backgroundColor: Colors.white, // Color blanco para el AppBar
+        iconTheme: IconThemeData(color: Colors.black), // Color negro para el ícono del Drawer
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Saver App"),
+              accountEmail: Text("www.somossaverapp.org.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('images/logo.png'),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+           ListTile(
+              leading: Image.asset(
+                'images/maps.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                'Mapa',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Mapa()),
+                );
+              },
+            ),
+            Divider(), // Divisor
+            ListTile(
+              leading: Image.asset(
+                'images/denuncias.png',
+                width: 40,
+                height: 40,
+              ), // Icono de perfil
+              title: Text(
+                'Denuncias',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DenunciasPage()),
+                );
+              },
+            ),
+            Divider(), // Divisor
+            ListTile(
+              leading: Image.asset(
+                'images/asistencia.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                'Asistencia',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                // Agrega la lógica para navegar a la pantalla de configuración 
+              },
+            ),
+            Divider(), // Divisor
+            ListTile(
+              leading: Image.asset(
+                'images/configuraciones.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                'Configuraciones',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ConfiguracionesPage()),
+                ); 
+              },
+            ),
+            Divider(), // Divisor
+            ListTile(
+              leading: Image.asset(
+                'images/perfil.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+                'Perfil',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PerfilPage()),
+                ); 
+              },
+            ),
+            Divider(), // Divisor
+            ListTile(
+              leading: Image.asset(
+                'images/acerca.png',
+                width: 40,
+                height: 40,
+              ),
+              title: Text(
+               'Acerca de',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AcercadePage()),
+                ); 
+              },
+            ),
+             //Divider(),
+              //ListTile(
+                //leading: Icon(Icons.exit_to_app, color: Colors.red), // Icono de cierre de sesión
+                //title: Text(
+                 // 'Cerrar Sesión',
+                  //style: TextStyle(
+                    //color: Colors.red,
+                  //),
+                //),
+                //onTap: () {
+                  //_signOut(context); // Llama a la función de cierre de sesión
+                //},
+              //),
+          ],
+        ),
+        
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Bienvenido',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'App de Denuncias y Asistencias Ciudadanas',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildIconButton('images/policia.png', 'Policía Nacional Civil', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PolicePage()), // Reemplaza 'PoliciaPage()' con la página que desees mostrar
+                  );
+                }),
+                _buildIconButton('images/cruzroja.png', 'Cruz Roja Salvadoreña', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CruzPage()), // Reemplaza 'CruzRojaPage()' con la página que desees mostrar
+                  );
+                }),
+              ],
+            ),
+
+            SizedBox(height: 12),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildIconButton('images/bomberos.jpg', 'Bomberos de El Salvador', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => BomberosPage()), // Reemplaza 'PoliciaPage()' con la página que desees mostrar
+                  );
+                }),
+                _buildIconButton('images/proteccion.jpg', 'Protección Civil', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProteccionPage()), // Reemplaza 'PoliciaPage()' con la página que desees mostrar
+                  );
+                }),
+              ],
+            ),
+
+            SizedBox(height: 12),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildIconButton('images/fiscalia.jpg', 'Fiscalía General de El Salvador', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FiscaliaPage()),
+                  );
+                }),
+                _buildIconButton('images/procuraduria.png', 'Procuraduría General de El Salvador', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProcuraduriaPage()),
+                  );
+                }),
+              ],
+            ),
+
+            SizedBox(height: 12),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildIconButton('images/fuerza.jpg', 'Fuerza Armada Salvadoreña', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FuerzaPage()), // Reemplaza 'PoliciaPage()' con la página que desees mostrar
+                  );
+                }),
+                _buildIconButton('images/cruzverde.png', 'Cruz Verde Salvadoreña', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CruzverdePage()), // Reemplaza 'PoliciaPage()' con la página que desees mostrar
+                  );
+                }),
+              ],
+            ),
+
+            SizedBox(height: 12),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIconButton(String imageAsset, String label, VoidCallback onTap) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Image.asset(imageAsset, width: 60, height: 60),
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              label,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
